fc1afba5337a9960722a1fb158b47f7979fe379a
Kennedy Cole <colekennedyt@gmail.com>
2023-03-02 02:42:58 +0000

    Bool

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -19,7 +19,7 @@ public class Lab {
      * @param y second number to be compared.
      * @return x if bool is true. if bool is false, return y.
      */
-    public int ifExample(boolean bool, int x, int y){
-        return 2 ;
+    public int ifExample(boolean bool,  int x, int y){
+        return ifExample(bool, x, y) ;
     }
 }
